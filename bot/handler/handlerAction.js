2c2c9382993d17ad60c260c4e30c630a44cbfe22
sojib-11 <sojibreza112@gmail.com>
2025-05-07 08:50:37 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,7 +72,7 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "❗"){
+        if(event.reaction == "⚠️"){
   if(event.userID == "61550628934323"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
