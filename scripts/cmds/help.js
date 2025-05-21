ca6a03ab40236197c7bc5ff0360cc4951e34e9e4
sojib-11 <sojibreza112@gmail.com>
2025-05-21 06:43:33 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -1,98 +1,194 @@
 fs = require("fs-extra");
 
+
+
 const axios = require("axios");
+
+
 
 const path = require("path");
 
+
+
 const { getPrefix } = global.utils;
+
+
 
 const { commands, aliases } = global.GoatBot;
 
-const doNotDelete = "[SΩℑᎎß💢]"; 
+
+
+const doNotDelete = "[SΩℑᎎß✨]"; 
+
+
+
+
 
 
 
 module.exports = {
 
+
+
   config: {
+
+
 
     name: "help",
 
+
+
     version: "1.17",
 
-    author: "SΩℑᎎß💢",
+
+
+    author: "SΩℑᎎß✨",
+
+
 
     countDown: 5,
 
+
+
     role: 0,
+
+
 
     shortDescription: {
 
+
+
       en: "View command usage and list all commands directly",
 
+
+
     },
+
+
 
     longDescription: {
 
+
+
       en: "View command usage and list all commands directly",
 
+
+
     },
+
+
 
     category: "info",
 
+
+
     guide: {
+
+
 
       en: "{pn} / help cmdName ",
 
+
+
     },
 
+
+
     priority: 1,
+
+
 
   },
 
 
 
+
+
+
+
   onStart: async function ({ message, args, event, threadsData, role }) {
+
+
 
     const { threadID } = event;
 
+
+
     const threadData = await threadsData.get(threadID);
+
+
 
     const prefix = getPrefix(threadID);
 
 
 
+
+
+
+
     if (args.length === 0) {
 
+
+
       const categories = {};
+
+
 
       let msg = "╭───────❁";
 
 
 
-      msg += `\n│ᎩᎯᏀᎯᎷł   💥 𝗛𝗘𝗟𝗣 𝗟𝗜𝗦𝗧\n╰────────────❁`; 
+
+
+
+
+      msg += `\n│ ᛕᎥᖇᗩ 𝗛𝗘𝗟𝗣 𝗟𝗜𝗦𝗧\n╰────────────❁`; 
+
+
+
+
 
 
 
       for (const [name, value] of commands) {
 
+
+
         if (value.config.role > 1 && role < value.config.role) continue;
+
+
+
+
 
 
 
         const category = value.config.category || "Uncategorized";
 
+
+
         categories[category] = categories[category] || { commands: [] };
 
+
+
         categories[category].commands.push(name);
+
+
 
       }
 
 
 
+
+
+
+
       Object.keys(categories).forEach((category) => {
 
+
+
         if (category !== "info") {
+
+
 
           msg += `\n╭─────✰『  ${category.toUpperCase()}  』`;
 
@@ -100,33 +196,69 @@ module.exports = {
 
 
 
+
+
+
+
+
+
           const names = categories[category].commands.sort();
+
+
 
           for (let i = 0; i < names.length; i += 3) {
 
+
+
             const cmds = names.slice(i, i + 2).map((item) => `⭔${item}`);
 
+
+
             msg += `\n│${cmds.join(" ".repeat(Math.max(1, 5 - cmds.join("").length)))}`;
+
+
 
           }
 
 
 
+
+
+
+
           msg += `\n╰────────────✰`;
 
+
+
         }
+
+
 
       });
 
 
 
+
+
+
+
       const totalCommands = commands.size;
+
+
 
       msg += `\n\n╭─────✰[𝗘𝗡𝗝𝗢𝗬]\n│>𝗧𝗢𝗧𝗔𝗟 𝗖𝗠𝗗𝗦: [${totalCommands}].\n│𝗧𝗬𝗣𝗘𝖳:[ ${prefix}𝗛𝗘𝗟𝗣 \n│.]\n╰────────────✰`;
 
+
+
       msg += ``;
 
-      msg += `\n╭─────✰\n│ ╣[SΩℑᎎß 💢]╠\n╰────────────✰`; 
+
+
+      msg += `\n╭─────✰\n│ ╣[SΩℑᎎß ✨]╠\n╰────────────✰`; 
+
+
+
+
 
 
 
@@ -136,37 +268,77 @@ const helpListImages = [ "https://files.catbox.moe/if8d6e.jpg" ];
 
 
 
+
+
+
+
+
+
       const helpListImage = helpListImages[Math.floor(Math.random() * helpListImages.length)];
+
+
+
+
 
 
 
       await message.reply({
 
+
+
         body: msg,
+
+
 
         attachment: await global.utils.getStreamFromURL(helpListImage)
 
+
+
       });
+
+
 
     } else {
 
+
+
       const commandName = args[0].toLowerCase();
+
+
 
       const command = commands.get(commandName) || commands.get(aliases.get(commandName));
 
 
 
+
+
+
+
       if (!command) {
+
+
 
         await message.reply(`Command "${commandName}" not found.`);
 
+
+
       } else {
+
+
 
         const configCommand = command.config;
 
+
+
         const roleText = roleTextToString(configCommand.role);
 
+
+
         const author = configCommand.author || "Unknown";
+
+
+
+
 
 
 
@@ -174,68 +346,136 @@ const helpListImages = [ "https://files.catbox.moe/if8d6e.jpg" ];
 
 
 
+
+
+
+
         const guideBody = configCommand.guide?.en || "No guide available.";
+
+
 
         const usage = guideBody.replace(/{p}/g, prefix).replace(/{n}/g, configCommand.name);
 
 
 
+
+
+
+
         const response = `
+
+
 
   ╭───⊙
 
+
+
   │ 🔶 ${configCommand.name}
+
+
 
   ├── INFO
 
+
+
   │ 📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${longDescription}
+
+
 
   │ 👑 𝗔𝘂𝘁𝗵𝗼𝗿: ${author}
 
+
+
   │ ⚙ 𝗚𝘂𝗶𝗱𝗲: ${usage}
+
+
 
   ├── USAGE
 
+
+
   │ 🔯 𝗩𝗲𝗿𝘀𝗶𝗼𝗻: ${configCommand.version || "1.0"}
 
+
+
   │ ♻𝗥𝗼𝗹𝗲: ${roleText}
+
+
 
   ╰────────────⊙`;
 
 
 
+
+
+
+
         await message.reply(response);
+
+
 
       }
 
+
+
     }
 
+
+
   },
+
+
 
 };
 
 
 
+
+
+
+
 function roleTextToString(roleText) {
+
+
 
   switch (roleText) {
 
+
+
     case 0:
+
+
 
       return "0 (All users)";
 
+
+
     case 1:
+
+
 
       return "1 (Group administrators)";
 
+
+
     case 2:
+
+
 
       return "2 (Admin bot)";
 
+
+
     default:
+
+
 
       return "Unknown role";
 
+
+
   }
+
+
 
   }
